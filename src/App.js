66cf6d3784a8dca590f425f7263f033d2eb0fd68
Elvis Nobrega <elvismdnin@gmail.com>
2019-03-24 17:41:38 -0300

    Início da brincadeira com o react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Blog from './Blog';
+
+class App extends Component {
+  render() {
+    return (
+      <Blog />
+    );
+  }
+}
+
+export default App;
