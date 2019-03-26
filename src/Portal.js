71adaa4444f87b9c76a77edbeb76ae32a5b84126
Elvis Nobrega <elvismdnin@gmail.com>
2019-03-25 21:13:35 -0300

    Adding some functionalities, the base of Portal, the homepage

--- src/Portal.js
+++ src/Portal.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
