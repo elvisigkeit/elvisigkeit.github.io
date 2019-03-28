a046610e822d9935d06269a777a439a1eefb864a
Elvis Nobrega <elvismdnin@gmail.com>
2019-03-28 08:21:30 -0300

    Configuring the routes (that's why this file is for)

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import Portal from './Portal';
 import NotFound from './NotFound';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Academia from './academia/Academia';
+import SigninAcademia from './academia/SigninAcademia'
+import Realm from './realm/Realm';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
           <Switch>
-              <Route path="/" exact={true} component={Portal} />
+              <Route path="/" exact component={Portal} />
+              <Route path="/academia" exact component={Academia}/>
+              <Route path="/academia/signin" exact component={SigninAcademia}/>
+              <Route path="/realm" exact component={Realm}/>
               <Route path="*" component={NotFound} />
           </Switch>
       </ BrowserRouter>
