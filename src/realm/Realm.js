7f1a1cb95c8f875c7876baadb71e06d1f9bccd4e
Elvis Nobrega <elvismdnin@gmail.com>
2019-04-09 16:43:29 -0300

    Finishing the first page of the blog

--- src/realm/Realm.js
+++ src/realm/Realm.js
@@ -2,9 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import SvgIcon from '@material-ui/core/SvgIcon';
+import RealmToolbar from './RealmToolbar';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
@@ -17,6 +15,11 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Footer from '../Footer';
+import helloworldPic from './titlepic/helloworld.png';
+import bootPic from './titlepic/boot.png';
+import linkingPic from './titlepic/linking.png';
+import cursorPic from './titlepic/cursor.png';
+import printPic from './titlepic/print.png';
 
 import "../index.css"
 
@@ -39,12 +42,6 @@ const styles = theme => ({
         marginRight: 'auto',
       },
     },
-    toolbarMain: {
-        height: '120px',
-    },
-    toolbarTitle: {
-      flex: 1,
-    },
     postList: {
       marginBottom: '4%',
       padding: '2% 7% 2% 7%',
@@ -59,24 +56,21 @@ const styles = theme => ({
     post: {
       transform: 'scale(0.8)',
       margin: '2%',
+    },
+    postPic: {
+      border: '3px solid #555',
+      height: '50px',
+      width: '150px',
     }
 });
 
 const posts = [
-  {index: 0, image: '', title: 'The Hello World', summary: 'Does it work to develop OS in virtual machines?'},
-  {index: 1, image: '', title: 'Starting the system', summary: 'How could I bootstrap my system?'},
-  {index: 2, image: '', title: 'Writing on the screen', summary: 'How I write on the screen?'},
-  {index: 3, image: '', title: 'Making the screen blink', summary: 'How I define a cursor?'},
-  {index: 4, image: '', title: 'Linking the code', summary: 'How I make the program compile?'},
+  {index: 0, path: '', image: helloworldPic, title: 'The Hello World', summary: 'Does it work to develop OS in virtual machines?'},
+  {index: 1, path: '', image: bootPic, title: 'Starting the system', summary: 'How could I bootstrap my system?'},
+  {index: 2, path: '', image: printPic, title: 'Writing on the screen', summary: 'How I write on the screen?'},
+  {index: 3, path: '', image: cursorPic, title: 'Making the screen blink', summary: 'How I define a cursor?'},
+  {index: 4, path: '', image: linkingPic, title: 'Linking the code', summary: 'How I make the program compile?'},
 ]
-
-function HomeIcon(props) {
-    return (
-        <SvgIcon {...props}>
-        <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-        </SvgIcon>
-    );
-}
 
 function Realm(props) {
     const { classes } = props;
@@ -84,20 +78,7 @@ function Realm(props) {
         <React.Fragment>
         <CssBaseline />
         <MuiThemeProvider theme={ytheme}>
-        <Paper>
-          <div className={classes.layout}>
-            <div width="100%">
-              <Toolbar className={classes.toolbarMain}>
-                  <Typography component="h2" variant="h2" color="inherit" align="left" noWrap className={classes.toolbarTitle}>
-                    Realm OS
-                  </Typography>
-                  <IconButton align="right" component={Link} to="/">
-                    <HomeIcon style={{ fontSize: 40, color: "#555" }} />
-                  </IconButton>
-              </Toolbar>
-            </div>
-          </div>
-        </Paper>
+        <RealmToolbar/>
         <Grid container className={classes.mainTitle}>
           <Grid item md={3}/>
           <Grid item md={3}>
@@ -123,12 +104,12 @@ function Realm(props) {
               
             <List className={classes.postList}>
               {posts.map(post => (
-                <div>
+                <Link to={post.path} style={{textDecoration: 'none'}}>
                   <ListItem alignItems="flex-start">
                     <ListItemAvatar>
-                      <img alt={`PostIcon${post.index}`} src={post.image}/>
+                      <img className={classes.postPic} alt={`PostIcon${post.index}`} src={post.image}/>
                     </ListItemAvatar>
-                    <ListItemText
+                    <ListItemText style={{marginBottom: '1%', marginTop: '1%'}}
                       primary={post.title}
                       secondary={
                         <React.Fragment>
@@ -138,7 +119,7 @@ function Realm(props) {
                     />
                   </ListItem>
                   <Divider style={{marginBottom: '5%'}}/>
-                </div>
+                </Link>
               ))}
             </List>
           </Paper>
