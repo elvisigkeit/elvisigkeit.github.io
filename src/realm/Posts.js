4fd2027e5055b37f9421bc771c8880d81e24bc44
Elvis Nobrega <elvismdnin@gmail.com>
2019-04-14 15:43:38 -0300

    Finishing a version to deploy in githubpages

--- src/realm/Posts.js
+++ src/realm/Posts.js
@@ -11,16 +11,16 @@ const posts = [
         index: 0, image: helloworldPic, title: 'The Hello World', summary: 'Does it work to develop OS in virtual machines?', content: helloworld
     },
     {
-        index: 1, image: bootPic, title: 'Starting the system', summary: 'How could I bootstrap my system?', content: 'a'
+        index: 1, image: bootPic, title: 'Starting the system', summary: 'How could I bootstrap my system?', content: startingsystem
     },
     {
-        index: 2, image: printPic, title: 'Writing on the screen', summary: 'How I write on the screen?', content: 'b'
+        index: 2, image: printPic, title: 'Writing on the screen', summary: 'How I write on the screen?', content: writingonscreen
     },
     {
-        index: 3, image: cursorPic, title: 'Making the screen blink', summary: 'How I define a cursor?', content: 'c'
+        index: 3, image: cursorPic, title: 'Making the screen blink', summary: 'How I define a cursor?', content: creatingcursor
     },
     {
-        index: 4, image: linkingPic, title: 'Linking the code', summary: 'How I make the program compile?', content: 'd'
+        index: 4, image: linkingPic, title: 'Linking the code', summary: 'How I make the program compile?', content: linkingcode
     },
 ]
 
@@ -29,6 +29,38 @@ function helloworld () {
     <Typography variant="h6" style={{textAlign: 'justify'}} paragraph>
         The main objective of this project is to discover and show ways to play with 
         the harsh and complex challenge of coding from almost nothing
+    </Typography>
+  )
+}
+
+function startingsystem () {  
+  return (
+    <Typography variant="h6" style={{textAlign: 'justify'}} paragraph>
+        In construction
+    </Typography>
+  )
+}
+
+function writingonscreen () {  
+  return (
+    <Typography variant="h6" style={{textAlign: 'justify'}} paragraph>
+        In construction
+    </Typography>
+  )
+}
+
+function creatingcursor () {  
+  return (
+    <Typography variant="h6" style={{textAlign: 'justify'}} paragraph>
+        In construction
+    </Typography>
+  )
+}
+
+function linkingcode () {  
+  return (
+    <Typography variant="h6" style={{textAlign: 'justify'}} paragraph>
+        In construction
     </Typography>
   )
 }
